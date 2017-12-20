d4ee1d402bf36567b4ee23373bd2099df138d6c5
Gus Antoniassi <gus.antoniassi@gmail.com>
2017-12-20 14:09:12 -0200

    Tratamento de labels realizado, variáveis a fazer

--- src/App/Assembler/Assembler.php
+++ src/App/Assembler/Assembler.php
@@ -2,17 +2,22 @@
 namespace App\Assembler;
 
 use App\Instruction;
+use App\LookupTable\SymbolTable;
 use App\Stream\IOStream;
 
 class Assembler
 {
     private $output;
+    private $symbolTable;
 
     public function assemble(IOStream $file)
     {
+        $this->symbolTable = new SymbolTable();
 
         $this->output = $this->loadFile($file);
+        $this->parseLabels();
         $this->parseToBinary();
+
 
         return $this->output;
     }
@@ -21,18 +26,57 @@ class Assembler
         $output = '';
         
         while (!$fileStream->isEOF()) {
-            $line = $fileStream->readLine();
+            $line = trim($fileStream->readLine());
+
+            $commentStart = strpos($line, '//');
+            if ($commentStart !== FALSE) {
+                $line = trim(substr($line, 0, $commentStart));
+            }
+
+            if (empty($line))
+                continue;
+
             $output .= $line . PHP_EOL;
         }
 
+        $output = trim($output, PHP_EOL);
+        
         return $output;
+    }
+
+    private function findNextInstructionNumber($currentLineNumber, $instructions) {
+        for ($i = $currentLineNumber; $i < count($instructions); $i++) {
+            $line = $instructions[$i];
+            if (!empty($line) && $line[0] !== '(') {
+                return $i;
+            }
+        }
+
+        // Returns false if the next valid line is not found
+        return false;
+    }
+
+    private function parseLabels() {
+        $lines = explode(PHP_EOL, $this->output);
+
+        $lineNumber = 0;
+        foreach ($lines as $line) {
+            if ($line[0] === '(') {
+                // Remove the parenthesis
+                $label = substr($line, 1, -1);
+                // Find the next instruction that is not a label
+                $nextLine = $this->findNextInstructionNumber($lineNumber, $lines);
+                $this->symbolTable->set($label, $nextLine);
+            } else {
+                $lineNumber++;
+            }
+        }
     }
 
     private function parseToBinary() {
         $lines = explode(PHP_EOL, $this->output);
 
         $output = '';
-        $lineNumber = 0;
         $instructionFactory = new Instruction\Factory\InstructionFactory();
         
         foreach ($lines as $line) {
@@ -46,7 +90,9 @@ class Assembler
             if (empty($line))
                 continue;
 
-            $lineNumber++;
+            // TODO: Verificar se a instrução é uma variável (@FOO), verificar se já está na tabela e se não estiver inserir
+            // Talvez eu precise usar algum esquema tipo containers pra fazer essa parte, já que o ideal seria a A-instruction
+            // fazer o lookup na tabela sozinha
 
             $instruction = $instructionFactory->getInstruction($line);
 
--- src/App/LookupTable/SymbolTable.php
+++ src/App/LookupTable/SymbolTable.php
@@ -1,6 +1,43 @@
 <?php
 namespace App\LookupTable;
 
-class SymbolTable {
-    
+class SymbolTable
+{
+    private $symbolTable = [];
+    private $nextVariablePointer;
+
+    public function __construct()
+    {
+        // Add the standard values R0, R1, R2...
+        for ($i = 0; $i < 16; $i++) {
+            $this->symbolTable["R$i"] = "$i";
+        }
+
+        $this->symbolTable['SCREEN'] = '16384';
+        $this->symbolTable['KBD'] = '24576';
+        $this->symbolTable['SP'] = '0';
+        $this->symbolTable['LCL'] = '1';
+        $this->symbolTable['ARG'] = '2';
+        $this->symbolTable['THIS'] = '3';
+        $this->symbolTable['THAT'] = '4';
+
+        $this->nextVariablePointer = 16;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed Returns the value from the table, or false if not found
+     */
+    public function lookup($key) {
+        return $this->symbolTable[$key] ?? false;
+    }
+
+    public function set($key, $value) {
+        $this->symbolTable[$key] = $value;
+    }
+
+    /** Debug only **/
+    public function getTable() {
+        return $this->symbolTable;
+    }
 }
