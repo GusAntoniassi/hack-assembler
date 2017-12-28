5b11a5275e99cd65dc72b4f90c747c6c8decd82d
Gus Antoniassi <gus.antoniassi@gmail.com>
2017-12-28 14:10:23 -0200

    Inicio do desenvolvimento dos testes de unidade no projeto

---
+++ tests/App/Instruction/AInstructionTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Test;
+
+use App\Exception\InvalidInstructionException;
+use App\Instruction\AInstruction;
+use App\LookupTable\SymbolTable;
+use PHPUnit\Framework\TestCase;
+
+class AInstructionTest extends TestCase {
+    private $symbolTable;
+
+    protected function setUp() {
+        $this->symbolTable = new SymbolTable();
+    }
+
+    public function testGetBinaryCodeWithNewVariable() {
+        $this->symbolTable->lookupOrSet('foo');
+
+        $instruction = new AInstruction('@foo', $this->symbolTable);
+
+        // Primeira variável é sempre armazenada na posição 16
+        $expected = '0000000000010000';
+
+        $result = $instruction->getBinaryCode();
+
+        $this->assertEquals($expected, $result);
+    }
+    
+    public function testGetBinaryCodeWithSecondNewVariable() {
+        $this->symbolTable->lookupOrSet('foo');
+        $this->symbolTable->lookupOrSet('bar');
+
+        $instruction = new AInstruction('@bar', $this->symbolTable);
+
+        // Segunda variável seria armazenada na posição 17
+        $expected = '0000000000010001';
+
+        $result = $instruction->getBinaryCode();
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetBinaryCodeWithDefinedVariable() {
+        $this->symbolTable->set('foo', 255);
+
+        $instruction = new AInstruction('@foo', $this->symbolTable);
+
+        // 255 em binário
+        $expected = '0000000011111111';
+
+        $result = $instruction->getBinaryCode();
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetBinaryCodeWithDefaultVariable() {
+        $instruction = new AInstruction('@R1', $this->symbolTable);
+
+        $expected = '0000000000000001';
+
+        $result = $instruction->getBinaryCode();
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetBinaryCodeWithRegularNumber() {
+        $instruction = new AInstruction('@1', $this->symbolTable);
+
+        $expected = '0000000000000001';
+
+        $result = $instruction->getBinaryCode();
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetBinaryCodeThrowsExceptionOnNumberTooBig() {
+        $instruction = new AInstruction('@32768', $this->symbolTable);
+        $this->expectException(InvalidInstructionException::class);
+        
+        $instruction->getBinaryCode();
+    }
+
+    public function testGetBinaryCodeThrowsExceptionOnNegativeNumber() {
+        $instruction = new AInstruction('@-123', $this->symbolTable);
+        $this->expectException(InvalidInstructionException::class);
+
+        $instruction->getBinaryCode();
+    }
+}
---
+++ tests/App/LookupTable/SymbolTableTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Test;
+
+use App\LookupTable\SymbolTable;
+use PHPUnit\Framework\TestCase;
+
+class SymbolTableTest extends TestCase {
+
+    /**
+     * @dataProvider providerSymbolTableDefaultValues
+     */
+    public function testSymbolTableIsInitializedWithDefaultValues($key, $expectedValue) {
+        $symbolTable = new SymbolTable();
+        
+        $result = $symbolTable->lookup($key);
+        
+        $this->assertEquals($expectedValue, $result);
+    }
+
+    public function providerSymbolTableDefaultValues() {
+        return [
+            ['R0', '0'],
+            ['R1', '1'],
+            ['R2', '2'],
+            ['R3', '3'],
+            ['R4', '4'],
+            ['R5', '5'],
+            ['R6', '6'],
+            ['R7', '7'],
+            ['R8', '8'],
+            ['R9', '9'],
+            ['R10', '10'],
+            ['R11', '11'],
+            ['R12', '12'],
+            ['R13', '13'],
+            ['R14', '14'],
+            ['R15', '15'],
+            ['SCREEN', '16384'],
+            ['KBD', '24576'],
+            ['SP', '0'],
+            ['LCL', '1'],
+            ['ARG', '2'],
+            ['THIS', '3'],
+            ['THAT', '4']
+        ];
+    }
+}
